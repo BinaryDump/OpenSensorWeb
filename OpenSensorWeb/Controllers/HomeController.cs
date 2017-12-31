b2e7eaf37f34a0003ab1af2da76909153a65107b
BinaryDump <BinaryDump@LENOVOZ50-70>
2017-12-31 17:26:47 +0100

    Add to github

---
+++ OpenSensorWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using OpenSensorWeb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OpenSensorWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.TemperatureValues = DataHelper.Instance.GetJSONSensorValue(1, 200); //86400 = a day ( 86400 seconds ) 
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ OpenSensorWeb/Services/DataHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OpenSensorWeb.Models;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace OpenSensorWeb.Services
+{
+    public class DataHelper
+    {
+        OpenMonitorEntities ome = new OpenMonitorEntities();
+
+
+        #region ##Singleton##
+        private static DataHelper _instance;
+        private DataHelper() { }
+
+        public static DataHelper Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new DataHelper();
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        public string GetJSONSensorValue(int sensorModuleId, int lastXValues)
+        {
+            List<OpenMonitorValue> valueList = new List<OpenMonitorValue>();
+            valueList = ome.OpenMonitorValue.Where(x => x.ModuleId == sensorModuleId).OrderByDescending(x => x.CreatedDate).Take(lastXValues).ToList();
+
+
+            var json = JsonConvert.SerializeObject(valueList);
+            string jsonString = json.ToString();
+
+            if (valueList.Any())
+            {
+                return jsonString;
+            }
+
+            return string.Empty;
+        }
+        
+
+    }
+}
